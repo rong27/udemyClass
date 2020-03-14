e1365bf5e8a4cbd96a44979969bc6735ab269b4b
rong <becausemiss@hotmail.com>
2020-03-14 20:40:43 +0800

    revise list

--- webDeveloper/js/list.js
+++ webDeveloper/js/list.js
@@ -4,17 +4,37 @@ window.setTimeout(function () {
 
     while (input !== "quit") {
         //handle input
-        if (input === 'list'){
-            console.log(todos);
-        }
-        else if (input === "new") {
-            // ask for new todo
-            let newTodos = prompt("Enter New Todos");
-            // add to todos array
-            todos.push(newTodos);
+        if (input === 'list') {
+            showTodos();
+        } else if (input === "new") {
+            addTodo();
+        } else if (input === "delete") {
+            deleteTodo();
         }
         // ask again for new input
         input = prompt("What would you like to do? ");
     }
     console.log("OK, You Quit the app.");
 }, 500)
+
+function showTodos() {
+        todos.forEach(function (todo, item) {
+            console.log("**********");
+            console.log(item + ": " + todo);
+    })
+    console.log("**********");
+}
+
+function addTodo() {
+    // ask for new todo
+    let newTodos = prompt("Enter New Todos");
+    // add to todos array
+    todos.push(newTodos);
+    console.log("Added it done")
+}
+
+function deleteTodo() {
+    let index = prompt("which index of todos do your want to delete?");
+    todos.splice(index, 1);
+    console.log("Removed it done!");
+} 
